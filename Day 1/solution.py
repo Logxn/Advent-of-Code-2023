7450995093a144092cf0dbc704190eecec84ab46
Logan Thompson <hello@loganthompson.de>
2023-12-01 21:01:41 +0100

    Added comments for day 1 & clean up

--- Day 1/solution.py
+++ Day 1/solution.py
@@ -1,8 +1,15 @@
 import re
 
+# I am to stupid for regex - don't hate me
 PATTERN_ONE = r'\d'
 PATTERN_TWO = r'(\d|one|two|three|four|five|six|seven|eight|nine)'
 NUMB_DICT = {"one": "1", "two": "2", "three": "3", "four": "4", "five":"5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+# We want to get the first and last number
+# For solution two we also have to consider number words like 'one'
+# Because we can have overlapping numbers 'eightwothree'
+# we scan the line from the beginning until we hit the first number and
+# scan the line from the end backwards until we hit the first number
 
 def get_match(line, reverse=False, solution=1):
     if solution == 1:
@@ -19,9 +26,9 @@ def get_match(line, reverse=False, solution=1):
                 continue
 
             if not matches[0].isnumeric():
-                return [NUMB_DICT[matches[0]], i]
+                return NUMB_DICT[matches[0]]
             
-            return [matches[0], i]
+            return matches[0]
     else:
         for i in range(len(line)):
             sub_str = line[:i]
@@ -31,9 +38,9 @@ def get_match(line, reverse=False, solution=1):
                 continue
             
             if not matches[0].isnumeric():
-                return [NUMB_DICT[matches[0]], i]
+                return NUMB_DICT[matches[0]]
             
-            return [matches[0], i]
+            return matches[0]
 
 with open('input.txt', 'r') as f:
     lines = f.readlines()
@@ -44,11 +51,7 @@ for line in lines:
     first_match = get_match(line)
     last_match = get_match(line, reverse=True)
 
-    if first_match[1] == last_match[1]:
-        solution += int(first_match[0] + last_match[0])
-        continue
-    
-    solution += int(first_match[0] + last_match[0])
+    solution += int(first_match + last_match)
 
 assert(solution == 55621)
 
@@ -58,10 +61,6 @@ for line in lines:
     first_match = get_match(line, solution=2)
     last_match = get_match(line, reverse=True, solution=2)
     
-    if first_match[1] == last_match[1]:
-        solution_two += int(first_match[0] + last_match[0])
-        continue
-
-    solution_two += int(first_match[0] + last_match[0])
+    solution_two += int(first_match + last_match)
 
 assert(solution_two == 53592)    
