572446e0dce16088542f16a41188a32c10426970
Logan Thompson <hello@loganthompson.de>
2023-12-02 13:25:36 +0100

    Part II RAW

--- Day 2/solution.py
+++ Day 2/solution.py
@@ -1,16 +1,6 @@
 max_countings = {'red': 12, 'green': 13, 'blue': 14}
 
-with open('input.txt', 'r') as f:
-    games = f.readlines()
-
-solution = 0
-
-for game in games:
-    game_info = game.split(': ')
-    game_id = game_info[0].split(' ')[1]
-    
-    is_possible = True
-
+def is_possible(game_info):
     rounds = game_info[1].split('; ')
     for round in rounds:
         draws = round.split(', ')
@@ -21,15 +11,48 @@ for game in games:
             amount = draw_info[0]
             
             if int(amount) > max_countings[color.replace('\n', '')]:
-                is_possible = False
-                break
-        
-        if not is_possible:
-            break
+                return False
+    return True
+            
+def get_power(game_info):
+    rounds = game_info[1].split('; ')
+    countings = {'red': 0, 'green': 0, 'blue': 0}
 
-    if not is_possible:
+    for round in rounds:
+        draws = round.split(', ')
+        
+        for draw in draws:
+            draw_info = draw.split(' ')
+            color = draw_info[1].replace('\n', '')
+            amount = int(draw_info[0])
+
+            if amount > countings[color]:
+                countings[color] = amount
+    
+    power = 1
+    for color in countings:
+        power *= countings[color]
+
+    return power
+
+with open('input.txt', 'r') as f:
+    games = f.readlines()
+
+solution = 0
+solution_two = 0
+
+for game in games:
+    game_info = game.split(': ')
+    game_id = game_info[0].split(' ')[1]
+    
+    solution_two += get_power(game_info)
+
+    if not is_possible(game_info):
         continue
 
     solution += int(game_id)
 
 print(solution)
+assert(solution == 2447)
+
+print(solution_two)
